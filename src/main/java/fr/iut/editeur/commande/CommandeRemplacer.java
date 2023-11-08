85487fcb7544914fca3a9fd2335f9eeeb5b6999b
gillet <paco.gillet@etu-umontpellier.fr>
2023-11-08 15:01:35 +0100

    Ajout commande remplacer
    finitions et tests

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if (parameters.length < 4) {   // on vérifie le respect du format
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        int depart = Integer.parseInt(parameters[1]);   // on récupère la position de départ
+        int fin = Integer.parseInt(parameters[2]);
+        String chaine = parameters[3];
+        this.document.remplacer(depart, fin, chaine);
+        super.executer();
+    }
+}
